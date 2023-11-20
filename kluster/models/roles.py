7ac04b69925003dc324ee8c558df9741f6a7871d
Freeman <freemanoparaocha@gmail.com>
2023-11-21 00:24:11 +0100

    users and role table

--- kluster/models/roles.py
+++ kluster/models/roles.py
@@ -1,6 +1,30 @@
+from kluster.models.base import BaseModel
 from kluster import db
 
-class Roles(db.Model):
-    id = db.Column(
-        db.String(60)
-    )
+
+class Roles(BaseModel):
+    __tablename__ = "roles"
+    role = db.Column(db.Enum('doctor', 'patient', name="USER_ROLE"), nullable=False)
+
+    def __init__(self, role):
+        """object constructor"""
+        super().__init__()
+        self.role = role
+
+    def __repr__(self):
+        """official object representation"""
+        return {
+            "id": self.id,
+            "role": self.role,
+            "createdAt": self.created_at,
+            "updatedAt": self.updated_at,
+        }
+
+    def format(self):
+        """Format the object's attributes as a dictionary"""
+        return {
+            "id": self.id,
+            "role": self.role,
+            "createdAt": self.created_at,
+            "updatedAt": self.updated_at,
+        }
