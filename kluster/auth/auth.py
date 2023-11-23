877edea695c2cdf602b7cdb42565693037c69655
Freeman <freemanoparaocha@gmail.com>
2023-11-23 13:55:14 +0100

    import optimization

--- kluster/auth/auth.py
+++ kluster/auth/auth.py
@@ -1,16 +1,19 @@
-import json, os, requests, datetime
+import datetime
+import json
+import os
+import requests
 from typing import Dict
+
 from flask import Blueprint, request, jsonify, redirect, url_for
-from kluster.models.users import Users
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, current_user, get_jwt_identity
+from oauthlib.oauth2 import WebApplicationClient
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from kluster import jwt
+from kluster.helpers import convert_pic_to_link, query_one_filtered
 from kluster.models.profiles import Profiles
 from kluster.models.roles import Roles
-from werkzeug.security import generate_password_hash, check_password_hash
-from kluster import jwt
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, current_user, get_jwt_identity
-
-from kluster.helpers import convert_pic_to_link, query_one_filtered
-from oauthlib.oauth2 import WebApplicationClient
-
+from kluster.models.users import Users
 
 auth = Blueprint("authentication", __name__, url_prefix="/api/v1/auth")
 
