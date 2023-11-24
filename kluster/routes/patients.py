8b2d399ce825d6d7cdeb6adb2ed540087ab58fd4
Freeman <freemanoparaocha@gmail.com>
2023-11-24 15:22:59 +0100

    medications all for a time

--- kluster/routes/patients.py
+++ kluster/routes/patients.py
@@ -14,10 +14,9 @@ MEDICATIONS
 
 @patients.route("/<user_id>/medications", methods=["GET"])
 # @jwt_required()
-def medications(user_id: str) -> tuple[Response, int] | dict[Any, Any]:
+def medications(user_id: str) -> tuple[Response, int] | Response | dict[Any, Any]:
     # print(type(user_id))
     date = request.args.get("date", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
-    print(type(date))
     if not isinstance(user_id, str):
         return jsonify(
             {
@@ -25,8 +24,36 @@ def medications(user_id: str) -> tuple[Response, int] | dict[Any, Any]:
                 "Error": "Bad Request",
             }
         ), 400
+    try:
+        # Attempt to convert the date string to a datetime object
+        date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    except ValueError:
+        # If there's an issue with the format or conversion, handle the error
+        return jsonify({
+            "message": "Invalid date format",
+            "Error": "Bad Request",
+        }), 400
 
-    # try:
-    medications_obj = query_all_filtered(Medication, patient_id=user_id)
-    print(medications_obj)
-    return {}
+    try:
+        # GET THE ALL THE MEDICATIONS FOR A PATIENT WITHIN A GIVEN DATE
+        medications_obj = Medication.query.filter(
+            Medication.patient_id == user_id,
+            Medication.start_date <= date,
+            date <= Medication.end_date
+        ).all()
+        medication_dict = [meds.to_dict() for meds in medications_obj]
+        return jsonify(
+            {
+                "message": "successful",
+                "date": medication_dict
+            }
+        ), 200
+    except Exception as exc:
+        print(exc)
+        return jsonify(
+            {
+                "message": "something went wrong",
+                "error": "Internal server error"
+            }
+        ), 500
+
