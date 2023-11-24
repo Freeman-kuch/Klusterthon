3c128f1baded71633b8706b2c565d5ef712090d7
Freeman <freemanoparaocha@gmail.com>
2023-11-24 18:45:41 +0100

    some more routes

--- kluster/routes/patients.py
+++ kluster/routes/patients.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, Response
 from flask_jwt_extended import jwt_required
-from kluster.helpers import query_all_filtered
+from kluster.helpers import query_one_filtered
 from kluster.models.medication import Medication
 from datetime import datetime
 from typing import Dict, Tuple, Any
@@ -13,9 +13,18 @@ MEDICATIONS
 
 
 @patients.route("/<user_id>/medications", methods=["GET"])
-# @jwt_required()
 def medications(user_id: str) -> tuple[Response, int] | Response | dict[Any, Any]:
-    # print(type(user_id))
+    """
+    Retrieves medication information for a specific patient based on the provided user ID and date.
+
+    Args:
+        user_id (str): The ID of the patient for whom to retrieve medication information.
+
+    Returns:
+        tuple[Response, int] | Response | dict[Any, Any]: If successful, returns a JSON response with a success message and a list of medication dictionaries.
+            If the user_id or date is invalid, returns a JSON response with an error message and a 400 Bad Request status code.
+            If an internal server error occurs, returns a JSON response with an error message and a 500 Internal Server Error status code.
+    """
     date = request.args.get("date", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     if not isinstance(user_id, str):
         return jsonify(
@@ -25,17 +34,15 @@ def medications(user_id: str) -> tuple[Response, int] | Response | dict[Any, Any
             }
         ), 400
     try:
-        # Attempt to convert the date string to a datetime object
-        date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+        date_val = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
     except ValueError:
-        # If there's an issue with the format or conversion, handle the error
         return jsonify({
             "message": "Invalid date format",
             "Error": "Bad Request",
         }), 400
 
     try:
-        # GET THE ALL THE MEDICATIONS FOR A PATIENT WITHIN A GIVEN DATE
+        # Change the return of the prescribed by to show the name of the doctor
         medications_obj = Medication.query.filter(
             Medication.patient_id == user_id,
             Medication.start_date <= date,
@@ -56,4 +63,67 @@ def medications(user_id: str) -> tuple[Response, int] | Response | dict[Any, Any
                 "error": "Internal server error"
             }
         ), 500
+
+
+
+@patients.route("/<user_id>/medications/<medication_id>", methods=["GET", "DELETE"])
+def medication(user_id: str, medication_id: str) -> tuple[Response, int] | Response | dict[Any, Any]:
+    """Gets a users medication by an indicated medication id
+
+    Args:
+        user_id (str): The ID of the  Patiend
+        medication_id (str): The ID of the medication to be gotten
+
+    Returns:
+        tuple[Response, int] | Response | dict[Any, Any]: _description_
+    """
+    if not isinstance(user_id, str) or not isinstance(medication_id, str):
+        return jsonify(
+            {
+                "message": "Invalid ID type passed, expecting UUID",
+                "error": "Bad Request"
+            }
+        )
+    if request.method == "GET":
+        try:
+            medication_obj = Medication.query.filter(
+                Medication.id == medication_id,
+                Medication.patient_id == user_id,
+                ).first_or_404("The Requested Medication was not found.")
+            return jsonify({
+                "data": medication_obj.to_dict(),
+                "message": "successful"
+                }
+                ), 200
+        except Exception as exc:
+            print(exc)
+            return jsonify(
+                {
+                    "error": "Internal Server Error",
+                    "message": "Something went wrong"
+                }
+            ), 500
+    
+    try:
+        medication_obj = query_one_filtered(Medication, id=medication_id, patient_id=user_id)
+        # medication_obj = Medication.query.filter(
+        #     Medication.id == medication_id,
+        #     Medication.patient_id == user_id,
+        # ).first_or_404("Not Found")
+        print(medication_obj.to_dict())
+        print(type(medication_obj))
+        medication_obj.delete()
+        return jsonify(
+            {
+                "message": "deleted"
+            }
+        ), 204
+    except Exception as e:
+        print(e)
+        return jsonify(
+            { 
+                "error": "Something went wrong",
+                "message": "Internal Server Error" 
+             }
+            ), 500
 
