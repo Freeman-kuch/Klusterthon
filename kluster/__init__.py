095e51a5bbd51f736d8c20a9e3a77f9e44eb00c3
KimFarida <farimomoh@gmail.com>
2023-11-26 20:29:46 +0100

    Fix: Celery Backend URL Issue

--- kluster/__init__.py
+++ kluster/__init__.py
@@ -14,7 +14,7 @@ jwt = JWTManager()
 def make_celery(app):
     celery = Celery(
         app.import_name, 
-        backend=app.config['CELERY_RESULT_BACKEND'], 
+        #backend=app.config['CELERY_RESULT_BACKEND'], 
         broker=app.config['CELERY_BROKER_URL']
     )
     celery.conf.update(app.config)
